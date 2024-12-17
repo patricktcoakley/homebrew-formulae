0d704b386d0795ca0c93442f22711bb6fc2d140a
Patrick T Coakley <contact@patricktcoakley.com>
2024-12-17 14:52:23 -0500

    Formatting

--- gdvm.rb
+++ gdvm.rb
@@ -1,8 +1,8 @@
 class Gdvm < Formula
-  desc "A Godot version manager focused on simplicity."
   homepage "https://github.com/patricktcoakley/gdvm"
-  url "https://github.com/patricktcoakley/gdvm/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "a050503954adb712e97bd0aecc464e5bc16476196b5e1aa1f021b1369a138198"
+  desc "Godot version manager focused on simplicity"
+  url "https://github.com/patricktcoakley/gdvm/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "2babe05fb6e549f26eadb21e49ed942f7ed32de12aac10663f6ade23b9c278b4"
   license "MIT"
   head "https://github.com/patricktcoakley/gdvm.git", branch: "main"
 
@@ -13,10 +13,10 @@ class Gdvm < Formula
 
     system "dotnet", "publish", "GDVM",
            "-c", "Release",
-           "--output", buildpath/"output"
+           "--output", buildpath / "output"
 
-    bin.install buildpath/"output/gdvm"
-          end
+    bin.install buildpath / "output/gdvm"
+  end
 
   test do
     assert_match "Usage:", shell_output("#{bin}/gdvm --help")
