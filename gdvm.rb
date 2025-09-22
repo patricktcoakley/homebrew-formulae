10b52839f3220ff67b275da3c16bdc0b84ed4d85
Anka <runner@sjc20-cw715-3cf7e5cd-9093-4a1d-847d-1fc68cce5679-B231CBED87B5.local>
2025-09-22 21:00:35 +0000

    gdvm 1.2.5

--- gdvm.rb
+++ gdvm.rb
@@ -1,8 +1,8 @@
 class Gdvm < Formula
   desc "Godot version manager focused on simplicity"
   homepage "https://github.com/patricktcoakley/gdvm"
-  url "https://github.com/patricktcoakley/gdvm/archive/refs/tags/v1.2.4.tar.gz"
-  sha256 "f409799e356c3c88d38da736b055a540b0837693aab2182e8cefa8c03f564e55"
+  url "https://github.com/patricktcoakley/gdvm/archive/refs/tags/v1.2.5.tar.gz"
+  sha256 "d266dd4865a53a743a6a14d09605b58e0fcee6f21a4e52b5f9e403020cdd681f"
   license "MIT"
   head "https://github.com/patricktcoakley/gdvm.git", branch: "main"
 
