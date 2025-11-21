eec6b5c04a85d815132dd5683761f3e7899024c4
Patrick T Coakley <contact@patricktcoakley.com>
2025-11-20 21:47:39 -0500

    Add fgvm

---
+++ fgvm.rb
@@ -0,0 +1,23 @@
+class Fgvm < Formula
+  desc "A friendly Godot version manager"
+  homepage "https://github.com/patricktcoakley/fgvm"
+  url "https://github.com/patricktcoakley/fgvm/archive/refs/tags/v2.0.0.tar.gz"
+  license "MIT"
+  head "https://github.com/patricktcoakley/fgvm.git", branch: "main"
+
+  depends_on "dotnet" => :build
+
+  def install
+    ENV["DOTNET_CLI_TELEMETRY_OPTOUT"] = "true"
+
+    system "dotnet", "publish", "Fgvm.Cli",
+           "-c", "Release",
+           "--output", buildpath / "output"
+
+    bin.install buildpath / "output/fgvm"
+  end
+
+  test do
+    assert_match "Usage:", shell_output("#{bin}/fgvm --help")
+  end
+end
