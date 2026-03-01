54de675e27c485efa8d7254086e1924fab9c5ecf
Anka <runner@sat12-bq159-2d910350-288f-4de4-99a5-3f028c814d28-9EE86E2A5605.local>
2026-03-01 18:41:51 +0000

    fgvm 2.0.2

--- Formula/fgvm.rb
+++ Formula/fgvm.rb
@@ -1,8 +1,8 @@
 class Fgvm < Formula
   desc "A friendly Godot version manager"
   homepage "https://github.com/patricktcoakley/fgvm"
-  url "https://github.com/patricktcoakley/fgvm/archive/refs/tags/v2.0.1.tar.gz"
-  sha256 "c912e0a22f90192e37b5f9975e08b70e86ef0374f0c4a47a3386258df2be7199"
+  url "https://github.com/patricktcoakley/fgvm/archive/refs/tags/v2.0.2.tar.gz"
+  sha256 "d10c637169b2f409720e13144a237f1a5374153809917851246716b6bf9bfca7"
   license "MIT"
   head "https://github.com/patricktcoakley/fgvm.git", branch: "main"
 
